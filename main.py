5ab69dcce64d1f131c84d38faaf41d3ebfa369d4
João Gabriel <128388946+JoaoGAO@users.noreply.github.com>
2023-05-10 21:44:17 +0000

    Resolução do exercício

--- main.py
+++ main.py
@@ -3,11 +3,30 @@
 
 def is_prime(number: int) -> bool:
     """Retorna True se o número for primo e False caso contrário."""
-
-    return False
-
+    if number <= 0:
+        return False
+    else:
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                return False
+        return True
 
 def list_prime_factors(number: int) -> list[int]:
     """Retorna uma lista com os fatores primos de cada número da lista fornecida."""
+    if number <= 1:
+        return []
 
-    return []
+    prime_factors = []
+    factor = 2
+    while factor <= int(number**0.5):
+        if number % factor == 0 and is_prime(factor):
+            count = 0
+            while number % factor == 0:
+                count += 1
+                number //= factor
+            prime_factors.extend([factor]*count)
+        factor += 1
+    if number > 1 and is_prime(number):
+        prime_factors.append(number)
+
+    return prime_factors
